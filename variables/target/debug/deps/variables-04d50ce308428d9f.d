777218de2f53b9ce80a2ac5a60adf83cde82e709
Raúl Moncada <raulemoncadat@gmail.com>
2025-07-05 16:26:30 -0500

    Remove some folders

--- variables/target/debug/deps/variables-04d50ce308428d9f.d
+++
@@ -1,5 +0,0 @@
-/home/rebelskvll/Proyectos/basic_rust/variables/target/debug/deps/variables-04d50ce308428d9f: src/main.rs
-
-/home/rebelskvll/Proyectos/basic_rust/variables/target/debug/deps/variables-04d50ce308428d9f.d: src/main.rs
-
-src/main.rs:
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/home/rebelskvll/Proyectos/basic_rust/variables/target/debug/variables: /home/rebelskvll/Proyectos/basic_rust/variables/src/main.rs
