3ba8a7ab2a32a62c008c5dbf9350de9ae483b4ed
Raúl Moncada <raulemoncadat@gmail.com>
2025-07-05 16:23:14 -0500

    Add variables project

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,17 @@
 fn main() {
+    // Declare a mutable variable type String
+    let mut name : String = String::new(); 
+    let mut age : String = String::new();
+    // Print on screen
     println!("Write your name: ");
-
+    // Read from input user and save in a variable
+    std::io::stdin().read_line(&mut name).unwrap();
+    println!("Write your age: ");
+    std::io::stdin().read_line(&mut age).unwrap();
+    // Remove newline
+    let name = name.trim();
+    // Remove newline, convert to int
+    let age: u8 = age.trim().parse().expect("Please enter a valid number");
+    //Print with placeholder
+    println!("Welcome {name}, from {age} years old");
 }
