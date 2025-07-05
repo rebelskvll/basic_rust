62049d4a6073b84831f48637e7059c4cccb040a6
Raúl Moncada <raulemoncadat@gmail.com>
2025-07-05 16:27:12 -0500

    Remove some folders

--- hello_world/target/debug/deps/hello_world-d73cedcabe85f7e8.d
+++
@@ -1,5 +0,0 @@
-/home/rebelskvll/Proyectos/basic_rust/hello_world/target/debug/deps/hello_world-d73cedcabe85f7e8: src/main.rs
-
-/home/rebelskvll/Proyectos/basic_rust/hello_world/target/debug/deps/hello_world-d73cedcabe85f7e8.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/home/rebelskvll/Proyectos/basic_rust/hello_world/target/debug/hello_world: /home/rebelskvll/Proyectos/basic_rust/hello_world/src/main.rs
